ea3345265c1e5b275724b15619ccf30fb0aeea29
Leandro <leandro.sousa@bgmrodotec.com.br>
2017-12-13 15:02:14 -0200

    Ajustes routing ui kit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,11 @@ import { RouterModule } from '@angular/router';
 import { UiKitComponent } from './ui-kit/ui-kit.component';
 
 const routes = [
-    // {
-    //     path: 'UiKit',
-    //     component: UiKitComponent
-    // },
-    // { path: '**', component: UiKitComponent }
+    {
+        path: 'UiKit',
+        component: UiKitComponent
+    },
+    { path: '**', component: UiKitComponent }
 ];
 
 export const AppRouting = RouterModule.forRoot(routes, {
